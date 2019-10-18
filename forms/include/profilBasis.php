b082cd9c9a047c1750da66b3177f81946c22f45e
Adrian Bieliński <Bielin1964@interia.pl>
2019-10-18 22:58:37 +0200

    change profil.php(1)

--- forms/include/profilBasis.php
+++ forms/include/profilBasis.php
@@ -3,60 +3,62 @@
 
 <head>    
 	<style>
-
-		#telefon, #mobil, #name, #vorname,
-		#strasse, #plz, #ort{
-			height: 30%;
-			width: 80%;
-			margin-left: 2%;
-			margin-bottom: 3%;
-			border: 1px solid #383838;
-			padding-left: 2%;
-			
+		.text-1{
+			display: flex;
+			margin-left: 4%;
 		}
+		#name, #vorname, #geburtsdatum, #beruf, #hiddenBeruf, #mobil, #telefon, #strasse, #plz, #ort{
+			margin-left: 15%;
+			border-radius: 5px 20px 5px;
+  			border-color: #a4338a;
+		}
+		
+		
 	</style>
 	</head>
 
-</br>
-<p>Name</p>
-<input type="text" id="name" name="name" placeholder="Mustermann" required>
 
-</br>
-<p>Vorname</p>
-<input type="text" id="vorname" name="vorname" placeholder="Max" required>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Name:</p>
+	<input type="text" id="name" name="name" placeholder="Mustermann" required>
+</div>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Vorname:</p>
+	<input type="text" id="vorname" name="vorname" placeholder="Max" required>
+</div>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Telefonnummer:</p>
+	<input type="text" id="telefon" name="telefon" placeholder="inklusive Vorwahl">
+</div>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Mobilfunknummer:</p>
+	<input type="text" id="mobil" name="mobil" placeholder="inklusive Vorwahl">
+</div>
 
-</br>
-<p>Telefonnummer</p>
-<input type="text" id="telefon" name="telefon" placeholder="inklusive Vorwahl">
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Strasse:</p>
+	<input type="text" id="strasse" name="strasse" placeholder="inklusive Hausnummer">
+</div>
 
-</br>
-<p>Mobilfunknummer</p>
-<input type="text" id="mobil" name="mobil" placeholder="inklusive Vorwahl">
-
-
-</br>
-<p>Strasse</p>
-<input type="text" id="strasse" name="strasse" placeholder="inklusive Hausnummer">
-
-
-</br>
-<p>Postleitzahl</p>
-<input type="text" id="plz" name="plz" placeholder="hjh">
-
-</br>
-<p>Ort</p>
-<input type="text" id="ort" name="ort" placeholder="hhh">
-
-</br>
-<p>Geburtsdatum</p>
-<input type="date" id="geburtsdatum" name="geburtsdatum" value="<?php echo date('Y-m-d'); ?>" required>
-
-</br>
-<p>beruflicher Status</p>
-<select id="beruf" name="beruf" placeholder="bitte wählen..." required onclick="newInput()">
-	<option value="angestellt">angestellt</option>
-	<option value="selbstständig">selbstständig</option>
-	<option value="arbeitssuchend">arbeitssuchend</option>
-	<option value="sonstiges">sonstiges</option> 
-</select>
-<input type="hidden" id="hiddenBeruf" name="altberuf" placeholder="bitte Beruf eingeben">
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Postleitzahl:</p>
+	<input type="text" id="plz" name="plz" placeholder="14478">
+</div>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Ort:</p>
+	<input  type="text" id="ort" name="ort" placeholder="Potsdam">
+</div>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Geburtsdatum:</p>
+	<input type="date" id="geburtsdatum" name="geburtsdatum" value="<?php echo date('Y-m-d'); ?>" required>
+</div>
+<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+	<p class="text-1">Beruflicher Status:</p>
+	<select id="beruf" name="beruf" placeholder="bitte wählen..." required onclick="newInput()">
+		<option value="angestellt">angestellt</option>
+		<option value="selbstständig">selbstständig</option>
+		<option value="arbeitssuchend">arbeitssuchend</option>
+		<option value="sonstiges">sonstiges</option> 
+	</select>
+	<input type="hidden" id="hiddenBeruf" name="altberuf" placeholder="bitte Beruf eingeben">
+</div>
--- forms/profil.php
+++ forms/profil.php
@@ -29,22 +29,15 @@
 			font-size: 		12px;
 			color: 			#383838;
 		}
-		
-	
-		#wrapper{
-			margin-top: 30%;
-			margin-bottom: 30%;	
-		}
-			
-		#name, #vorname, #geburtsdatum, #beruf, #hiddenBeruf
+
+		#name, #vorname, #geburtsdatum, #beruf, #hiddenBeruf, #mobil, #telefon, #strasse, #plz, #ort
 		{
 			height: 30%;
-			width: 80%;
+			width: 60%;
 			margin-left: 2%;
 			margin-bottom: 3%;
 			border: 1px solid #383838;
 			padding-left: 2%;
-			
 		}
 		
 		#geburtsdatum, #beruf{
@@ -67,15 +60,37 @@
 	</head>
 
 <body>
-
-	<div id="wrapper">
-			<nav class="navbar navbar-default navbar-fixed-top">
-			<div>
-				<h1>persönliche Daten</h1>
-			</div>
-		</nav>
-		<div class="row" id="profil">
-				<div class="col-md-4">
+	<div>
+		<span>
+			<h1 class="alert alert-success" style="width: 100vw;">Persönliche Daten</h1>  <!-- Anzeige im Header -->
+		</span>
+	</div>
+	<div id="wrapper" style="display: flex;">
+	<div class="navbar-collapse navbar-ex1-collapse" style="min-width: 20%; z-index: 1; padding-right: 20px;">
+                <ul class="nav navbar-nav side-nav" style="display: flex; flex-direction: column;">
+                    <li>
+                        <a href="../vertragsAuswahl.php"><i class="glyphicon glyphicon-th"></i><span class="text">Vertrag Auswahl</span><i class="fa fa-fw fa-caret-down"></i></a>
+                    </li>                    
+                    <li>
+                        <a href="../vertragsUebersicht.php"><i class="glyphicon glyphicon-list-alt"></i><span class="text">Vertrag Ubersicht</span><i class="fa fa-fw fa-caret-down"></i></a>
+                    </li>
+                    <li>
+                        <a href="pDaten.php"><i class="glyphicon glyphicon-user"></i><span class="text">Profil</span><i class="fa fa-fw fa-caret-down"></i></a>
+                    </li>
+                     <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="glyphicon glyphicon-cog"></i><span class="text">Einstellungen</span><i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+							<li class="nav-item"><a href="vertragspartner.php"><i class="glyphicon glyphicon-pushpin"></i> <span>Partner hinzufügen</span></a></li>
+							<li class="nav-item"><a href="datenschutz.php"><i class="glyphicon glyphicon-info-sign"></i> <span>Datenschutz</span></a></li>
+							<li class="nav-item"><a href="agb.php"><i class="glyphicon glyphicon-info-sign"></i> <span>AGB</span></a></li>
+							<li class="nav-item"><a href="faq.php"><i class="glyphicon glyphicon-info-sign"></i> <span>FAQ</span></a></li>
+							<li class="nav-item"><a href="logout.php"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+    	</div>
+		<div class="row" id="profil"style="width: 100vw; display:flex;">
+				<div class="col-md-12" style="display: flex; justify-content: center; position: relative; ">
 
 					<?php 
 						$statement = $pdo->prepare("SELECT * FROM user WHERE ID = ".$_SESSION['userID']);
@@ -84,10 +99,11 @@
 					?>
 		
     
-					<form action="sendRequest.php" method="post">		
-						
-						<p>E-Mail:<?php echo "</br> ".$userData['E_Mail']; ?></p>
-						
+					<form style="display: flex; justify-content: center; flex-direction: column;" action="sendRequest.php" method="post">		
+						<div class="row" style="display: flex; width: 80vw; flex-direction: column;">
+							<p class="text-1">E-Mail:</p>
+							<h5 class="text-1" style="padding-left: 5%;"><?php echo "</br> ".$userData['E_Mail']; ?></h5>
+						</div>
 						
 						<?php include('include/profilBasis.php'); ?>
 						
@@ -112,7 +128,9 @@
 				</div>
 		</div>
 	</div>
-	<?php
-		include('include/footer.php');
-	?>
+	<script src="../vendor/jquery/jquery.min.js"></script>
+	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="../vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="../js/klorofil-common.js"></script>
 </body>
+</html>
